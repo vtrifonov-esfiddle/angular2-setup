dac71d6dd8f5dc75a74621affb336f1ad9885cae
vladotrifonov@gmail.com <vladotrifonov@gmail.com>
2016-07-12 15:20:06 +0100

    Initial angular 2.0 setup

---
+++ Angular2Setup/Angular2Setup/gulpfile.js
@@ -0,0 +1,50 @@
+﻿/// <binding AfterBuild='default' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var ts = require('gulp-typescript');
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+
+var destPath = './wwwroot/libs/';
+
+// Delete the dist directory
+gulp.task('clean', function () {
+    return gulp.src(destPath)
+        .pipe(clean());
+});
+
+gulp.task("scriptsNStyles", () => {
+    gulp.src([
+            'core-js/client/**',
+            'systemjs/dist/system.src.js',
+            'reflect-metadata/**',
+            'rxjs/**',
+            'zone.js/dist/**',
+            '@angular/**',
+            'jquery/dist/jquery.*js',
+            'bootstrap/dist/js/bootstrap.*js',
+    ], {
+        cwd: "node_modules/**"
+    })
+        .pipe(gulp.dest("./wwwroot/libs"));
+});
+
+var tsProject = ts.createProject('tsconfig.json');
+gulp.task('ts', function (done) {
+    var tsResult = gulp.src([
+            "app/*.ts"
+    ])
+        .pipe(ts(tsProject), undefined, ts.reporter.fullReporter());
+    return tsResult.js.pipe(gulp.dest('./wwwroot/app'));
+});
+
+gulp.task('watch', ['watch.ts']);
+
+gulp.task('watch.ts', ['ts'], function () {
+    return gulp.watch('app/*.ts', ['ts']);
+});
+
+gulp.task('default', ['scriptsNStyles', 'ts']);
